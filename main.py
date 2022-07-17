1de7cd8e4a725ee638da3060f2dfbe4b83296921
cedric-hermans <109413872+cedric-hermans@users.noreply.github.com>
2022-07-17 23:02:14 +0200

    Added Game class

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Welcome to Hanged!")
+class Game:
+    def __init__(self, word):
+        self.word = word
+        self.guessCount = 0
+        self.isRunning = True
+
+print("""Welcome to Hanged!
+------------------
+Press [S]tart to begin a new game""")
+game = Game("test")

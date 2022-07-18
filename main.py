6851d553d754f273bc8626a58aa7a8dab54237cf
cedric-hermans <109413872+cedric-hermans@users.noreply.github.com>
2022-07-18 21:15:15 +0200

    Find and replace character
    but only with the first hit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ words = ["test", "python", "hangman"]
 class Game:
     def __init__(self, word):
         self.word = word
+        self.displayWord = ""
         self.guessCount = 0
         self.isRunning = True
 
@@ -13,6 +14,18 @@ print("""Welcome to Hanged!
 
 game = Game(random.choice(words))
 print("We got a word ready for you")
-print(game.word.replace(game.word, "_"*len(game.word)))
+game.displayWord = game.word.replace(game.word, "_"*len(game.word))
+print(game.displayWord)
+
+while game.guessCount <= 7:
+    print("Guess a letter:")
+    guess = input()
+    i = game.word.find(guess)
+    if i == -1:
+        print("Letter not found")
+    else:
+        print("Letter found")
+        game.displayWord = game.displayWord[0:i] + guess + game.displayWord[i+1:]
+        print(game.displayWord)
 
 

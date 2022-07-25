58f7693f35eaa9a7e05aead2e8198f3f75789254
cedric-hermans <109413872+cedric-hermans@users.noreply.github.com>
2022-07-25 20:53:08 +0200

    Added input check

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 request = requests.get("https://random-words-api.vercel.app/word")
 json_data = request.json()
-word = json_data[0]["word"]
+word = json_data[0]["word"].lower()
 
 class Game:
     def __init__(self, word):
@@ -114,7 +114,13 @@ print(game.displayWord)
 while game.guessCount < 7 and game.is_running:
     print("Guess a letter:")
     player_guess = input()
-    game.guess_character(player_guess)
+    if player_guess not in game.guessedCharacters\
+            and len(player_guess) == 1\
+            and player_guess.isdigit() == False:
+        game.guess_character(player_guess)
+    else:
+        print("Incorrect guess!")
+
 
 if game.guessCount < 7:
     print("You won!")
